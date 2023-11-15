8d4ade47b2458a06e2ed3ab83987e77bf0fa7157
magdalenahuget <magdalena.huget@gmail.com>
2023-11-16 00:56:28 +0100

    Feature: update request and response

--- src/main/java/com/company/solarwatch/model/payload/UserRequest.java
+++ src/main/java/com/company/solarwatch/model/payload/UserRequest.java
@@ -1,8 +1,12 @@
 package com.company.solarwatch.model.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class UserRequest {
     private String username;
     private String password;
